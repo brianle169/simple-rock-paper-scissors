bf15c4e02a460c1a60d1de90112805fc33b70347
Minh Le Cong <83758821+brianle169@users.noreply.github.com>
2023-03-29 18:53:53 +0700

    Add case-insensitive feature to ensure consistency

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   return options[choice];
 }
+
+function playRound(playerSelection) {
+  playerSelection = playerSelection[0]
+    .toUpperCase()
+    .concat(playerSelection.slice(1).toLowerCase());
+  return playerSelection;
+}
