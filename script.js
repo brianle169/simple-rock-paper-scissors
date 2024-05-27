bceebd1deb2e9bed62127e31d38e1f314b698bd4
Brian Le <83758821+brianle169@users.noreply.github.com>
2024-05-27 16:01:17 -0400

    Relocate victory condition checking from gameStart(e) function to playRound(playerSelection, computerSelection) functionto stop the game immediately after a player reaches 5 points.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const options = document.querySelectorAll(".options");
 const replayButton = document.querySelector("#replay");
 let playerSelection = "",
-   computerSelection = "";
+  computerSelection = "";
 let playerScore = 0,
-   computerScore = 0;
-let gameCounter = 0;
+  computerScore = 0;
+let roundCounter = 0;
 
 const result = document.querySelector(".result");
 const score = document.querySelector(".score");
@@ -12,71 +12,72 @@ score.textContent = `🧑🏻 ${playerScore} - ${computerScore} 🤖`;
 
 //Options buttons event listener
 options.forEach((option) => {
-   option.addEventListener("click", gameStart);
+  option.addEventListener("click", gameStart);
 });
 
 function gameStart(e) {
-   if (playerScore < 5 && computerScore < 5) {
-      gameCounter++;
-      getPlayerChoice(e);
-      getComputerChoice();
-      playRound(playerSelection, computerSelection);
-   } else {
-      announceWinner(playerScore, computerScore);
-      return;
-   }
+  roundCounter++;
+  getPlayerChoice(e);
+  getComputerChoice();
+  playRound(playerSelection, computerSelection);
+  return;
 }
 
 //Replay button listener
 replayButton.addEventListener("click", replay);
 
 function replay() {
-   playerSelection = "";
-   computerSelection = "";
-   computerScore = 0;
-   playerScore = 0;
-   gameCounter = 0;
-   result.textContent = "First to 5 wins!";
-   score.textContent = `🧑🏻 ${playerScore} - ${computerScore} 🤖`;
+  playerSelection = "";
+  computerSelection = "";
+  computerScore = 0;
+  playerScore = 0;
+  roundCounter = 0;
+  result.textContent = "First to 5 wins!";
+  score.textContent = `🧑🏻 ${playerScore} - ${computerScore} 🤖`;
 }
 
 function getPlayerChoice(e) {
-   playerSelection = e.target["textContent"];
-   return playerSelection;
+  playerSelection = e.target["textContent"];
+  return playerSelection;
 }
 
 function getComputerChoice() {
-   let options = ["Rock", "Paper", "Scissors"];
-   let choice = Math.floor(Math.random() * 3);
-   computerSelection = options[choice];
-   return computerSelection;
+  let options = ["Rock", "Paper", "Scissors"];
+  let choice = Math.floor(Math.random() * 3);
+  computerSelection = options[choice];
+  return computerSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
-   if (
-      (playerSelection === "Rock" && computerSelection === "Paper") ||
-      (playerSelection === "Paper" && computerSelection === "Scissors") ||
-      (playerSelection === "Scissors" && computerSelection === "Rock")
-   ) {
-      result.textContent = `Round ${gameCounter}: You Lose! ${computerSelection} beats ${playerSelection}`;
-      computerScore += 1;
-      score.textContent = `🧑🏻 ${playerScore} - ${computerScore} 🤖`;
-   } else if (playerSelection === computerSelection) {
-      result.textContent = `Round ${gameCounter}: Draw!`;
-   } else {
-      result.textContent = `Round ${gameCounter}: You Win! ${playerSelection} beats ${computerSelection}`;
-      playerScore += 1;
-      score.textContent = `🧑🏻 ${playerScore} - ${computerScore} 🤖`;
-   }
+  if (
+    (playerSelection === "Rock" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Rock")
+  ) {
+    result.textContent = `Round ${roundCounter}: You Lose! ${computerSelection} beats ${playerSelection}`;
+    computerScore += 1;
+    score.textContent = `🧑🏻 ${playerScore} - ${computerScore} 🤖`;
+  } else if (playerSelection === computerSelection) {
+    result.textContent = `Round ${roundCounter}: Draw!`;
+  } else {
+    result.textContent = `Round ${roundCounter}: You Win! ${playerSelection} beats ${computerSelection}`;
+    playerScore += 1;
+    score.textContent = `🧑🏻 ${playerScore} - ${computerScore} 🤖`;
+  }
+
+  // Check winner
+  if (playerScore === 5 || computerScore === 5) {
+    announceWinner(playerScore, computerScore);
+  }
 }
 
 function announceWinner(playerScore, computerScore) {
-   let winner = "";
-   winner =
-      playerScore > computerScore
-         ? "Player Wins! Congratulations"
-         : playerScore === computerScore
-         ? "Both Draw!"
-         : "Computer Wins! Better luck next time";
-   result.textContent = winner;
+  let winner = "";
+  winner =
+    playerScore > computerScore
+      ? "Player Wins! Congratulations"
+      : playerScore === computerScore
+      ? "Both Draw!"
+      : "Computer Wins! Better luck next time";
+  result.textContent = winner;
 }
